a3be870b0b239ae15aeffe9b689e74526eb08341
Nitish <pqnitish2020.com>
2025-07-27 19:54:35 +0530

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+let age = 28
+function displayAge(){
+    console.log(`display age before update ${age}`);
+    
+}
+function changeAge(){
+    let age = 30;
+    console.log(`after updating age ${age}`);
+    
+}
+displayAge()
+changeAge()
